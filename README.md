8a7ebb6ca1dfbfc0d2913c87b7a3853bd4475636
Brian Lam <Brian.Sun.Lam@gmail.com>
2023-09-06 18:28:22 -0700

    basic outline

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 ### This is the repository for the front facing side of Spring Roll House ###
+### The goal is to have a website up and running on WIX by 10/31/2023 ###
--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ function App() {
             <nav class="navbar background">
                 <ul class="nav-list">
                     <div class="logo">
-                        <img src=
-"https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210420155809/gfg-new-logo.png" />
+                        <img src="" /> /* This is where our logo would be */
                     </div>
-                    <li><a href="#courses">Courses</a></li>
-                    <li><a href="#tutorials">Tutorials</a></li>
-                    <li><a href="#jobs">Jobs</a></li>
-                    <li><a href='#student'>Student</a></li>
+                    <li><a href="#Egg Rolls">Egg Rolls</a></li>
+                    <li><a href="#Spring Rolls">Spring Rolls</a></li>
+                    <li><a href="#Catering">Catering</a></li>
+                    <li><a href='#Contact'>Contact</a></li>
+                    <li><a href='#About'>About</a></li>
                 </ul>
   
                 <div class="rightNav">
@@ -27,23 +27,17 @@ function App() {
                 <div class="box-main">
                     <div class="firstHalf">
                         <h1 class="text-big">
-                            7 Best Tips To Speed Up Your 
-                            Job Search in 2022
+                            Header
                         </h1>
                         <p class="text-small">
-                            Hunting down a relevant job requires 
-                            proper techniques for showcasing your 
-                            potential to the employer. But with 
-                            the advent of COVID-19, it has become 
-                            a bit challenging and competitive to 
-                            reach out for your dream job. Many 
-                            individuals have lost their jobs 
-                            during these times, and on the other 
-                            hand, freshers are facing difficulties
-                            while applying for a new job. But 
-                            there is no need for panic, you can 
-                            change your ways and streamline things 
-                            in a way that you get a proper result.
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis 
+                        aute irure dolor in reprehenderit in voluptate velit esse cillum 
+                        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+                        non proident, sunt in culpa qui officia deserunt mollit 
+                        anim id est laborum."
                         </p>
                     </div>
                 </div>
@@ -52,19 +46,17 @@ function App() {
                 <div class="box-main">
                     <div class="secondHalf">
                         <h1 class="text-big" id="program">
-                            JavaScript Tutorial
+                            Header
                         </h1>
                         <p class="text-small">
-                            JavaScript is the world most popular 
-                            lightweight, interpreted compiled 
-                            programming language. It is also 
-                            known as scripting  language for
-                            web pages. It is well-known for 
-                            the development of web page many 
-                            non-browser environments also use
-                            it. JavaScript can be used for 
-                            Client-side developments as well 
-                            as Server-side developments.
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis 
+                        aute irure dolor in reprehenderit in voluptate velit esse cillum 
+                        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+                        non proident, sunt in culpa qui officia deserunt mollit 
+                        anim id est laborum."
                         </p>
                     </div>
                 </div>
@@ -73,28 +65,17 @@ function App() {
                 <div class="box-main">
                     <div class="secondHalf">
                         <h1 class="text-big" id="program">
-                            Java Programming Language
+                            Header
                         </h1>
                         <p class="text-small">
-                            When compared with C++, Java codes 
-                            are generally more maintainable 
-                            because Java does not allow many 
-                            things which may lead to 
-                            bad/inefficient programming if used 
-                            incorrectly. For example, 
-                            non-primitives are always references
-                            in Java. So we cannot pass large
-                            objects (like we can do in C++) to 
-                            functions, we always pass references 
-                            in Java. One more example, since there
-                            are no pointers, bad memory access 
-                            is also not possible. When compared 
-                            with Python, Java kind of fits between
-                            C++ and Python. The programs are written
-                            in Java typically run faster than 
-                            corresponding Python programs and slower 
-                            than C++. Like C++, Java does static 
-                            type checking, but Python does not.
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis 
+                        aute irure dolor in reprehenderit in voluptate velit esse cillum 
+                        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+                        non proident, sunt in culpa qui officia deserunt mollit 
+                        anim id est laborum."
                         </p>
                     </div>
                 </div>
@@ -103,27 +84,23 @@ function App() {
                 <div class="box-main">
                     <div class="secondHalf">
                         <h1 class="text-big" id="program">
-                            What is Machine Learning?
+                            Header
                         </h1>
                         <p class="text-small">
-                            Machine Learning is the field of study 
-                            that gives computers the capability to 
-                            learn without being explicitly 
-                            programmed. ML is one of the most exciting
-                            technologies that one would have ever 
-                            come across. As it is evident from the 
-                            name, it gives the computer that makes 
-                            it more similar to humans: The ability 
-                            to learn. Machine learning is actively 
-                            being used today, perhaps in many more 
-                            places than one would expect.
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis 
+                        aute irure dolor in reprehenderit in voluptate velit esse cillum 
+                        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+                        non proident, sunt in culpa qui officia deserunt mollit 
+                        anim id est laborum."
                         </p>
                     </div>
                 </div>
             </section>
             <footer className="footer">
                 <p className="text-footer">
-                    Copyright ©-All rights are reserved
                 </p>
             </footer>
         </div>
